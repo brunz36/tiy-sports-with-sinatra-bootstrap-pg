f89d46f438994e7ce9d17a6e00f01af805fb6f3d
Bruno Ribeiro <brunz36@gmail.com>
2017-04-05 14:52:31 -0400

    Add enumeration to team panel

--- app.rb
+++ app.rb
@@ -2,10 +2,25 @@ require 'sinatra'
 require 'pg'
 require 'awesome_print'
 require 'sinatra/reloader' if development?
+require 'active_record'
 
-require_relative 'active_record_setup'
+ActiveRecord::Base.logger = Logger.new(STDOUT)
+ActiveRecord::Base.establish_connection(
+  adapter: "postgresql",
+  database: "tiy-sports"
+)
+
+after do
+  ActiveRecord::Base.connection.close
+end
+
+require_relative 'team'
+require_relative 'player'
+require_relative 'membership'
 
 get '/' do
+  @teams = Team.all
+  @players = Player.all
 
   erb :home
 end
